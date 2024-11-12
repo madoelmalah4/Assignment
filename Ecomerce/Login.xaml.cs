5499186bb225c4c9f0d6557e48237ef69ccb5614
madoelmalah4 <152697670+madoelmalah4@users.noreply.github.com>
2024-11-13 00:06:48 +0200

    project Features

---
+++ Ecomerce/Login.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ecomerce
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        EcomerceEntities Db = new EcomerceEntities();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(PasswordBox.Text == "" || EmailTextBox.Text == "")
+            {
+                return;
+            }
+            if(PasswordBox.Text == "1" && EmailTextBox.Text == "1")
+            {
+                this.NavigationService.Navigate(new Admin());
+                MessageBox.Show("Logged in Successfully as admin");
+                return;
+            }
+
+            var student = Db.Users.Where(user => user.userEmail == EmailTextBox.Text && user.userPassword == PasswordBox.Text).FirstOrDefault();
+
+            if(student != null)
+            {
+                MessageBox.Show("Logged in Successfully");
+                this.NavigationService.Navigate(new Products());
+            }
+            else
+            {
+                MessageBox.Show("Invalid User");
+            }
+        }
+    }
+}
---
+++ Ecomerce/Signup.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ecomerce
+{
+    /// <summary>
+    /// Interaction logic for Signup.xaml
+    /// </summary>
+    public partial class Signup : Page
+    {
+        EcomerceEntities Db = new EcomerceEntities();
+        public Signup()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(ConfirmPasswordBox.Text =="" || PasswordBox.Text == "" || EmailTextBox.Text == "" || NameTextBox.Text == "")
+            {
+                MessageBox.Show("Login Reqiured");
+                return;
+            }
+
+            User user = new User();
+
+            user.userName = NameTextBox.Text;
+            user.userPassword = PasswordBox.Text;
+            user.userEmail = EmailTextBox.Text;
+
+            Db.Users.Add(user);
+            Db.SaveChanges();
+            MessageBox.Show("Logged in Successfully");
+            this.NavigationService.Navigate(new Login());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Login());
+        }
+    }
+}
